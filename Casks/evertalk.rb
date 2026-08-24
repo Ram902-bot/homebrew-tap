64ba1879be385adf940739e08842ce705ae3b30c
Ramnarayanan G <ramnarayanan@Ramnarayanans-MacBook-Pro-2.local>
2026-08-24 19:48:37 +0530

    Fix asset URL to use versioned filename

--- Casks/evertalk.rb
+++ Casks/evertalk.rb
@@ -2,7 +2,7 @@ cask "evertalk" do
   version "2.1.0"
   sha256 "a0a70213d5979db99c04d30b5ed8a899db8463710d52f5ca641d8fc094f6d177"
 
-  url "https://github.com/Ram902-bot/evertalk/releases/download/v#{version}/Evertalk.zip"
+  url "https://github.com/Ram902-bot/evertalk/releases/download/v#{version}/Evertalk-#{version}.zip"
   name "Evertalk"
   desc "Privacy-first local voice-to-text for Mac"
   homepage "https://github.com/Ram902-bot/evertalk"
